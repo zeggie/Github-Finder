9bc8e2ca7e51d743cc4782d390e789cfb975980d
Drake <dngu043@hotmail.com>
2018-01-17 16:21:43 +1300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//Init Github
+const github = new GitHub;
+
+//Init UI
+const ui = new UI;
+
+//  Search input
+const searchUser = document.getElementById('searchUser');
+
+//  Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  //  Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    //Make http call
+    github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        //Show Alert
+        ui.showAlert('User not found' ,'alert alert-danger');
+      } else {
+        //Show Profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    //Clear Profile
+    ui.clearProfile();
+  }
+})
---
+++ js/github.js
@@ -0,0 +1,22 @@
+class GitHub {
+  constructor(){
+    this.client_id = '25dedf24554e8b8e5e88';
+    this.client_secret = '50cbc368d750490c607de71759975944b9a8b80d';
+    this.repo_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
